98a6d5c03ad6808b018cedf8f7a4f8e554d9efe3
Manish Raghavan <mraghavan@holylogin01.rc.fas.harvard.edu>
2022-01-08 02:02:31 -0500

    reduced time

--- code/sample_script.sh
+++ code/sample_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH -c 4                # Number of cores (-c)
-#SBATCH -t 0-3:00          # Runtime in D-HH:MM, minimum of 10 minutes
+#SBATCH -t 0-1:00          # Runtime in D-HH:MM, minimum of 10 minutes
 #SBATCH -p serial_requeue   # Partition to submit to
 #SBATCH --mem=5000           # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH -o out_files/samp.out  # File to which STDOUT will be written, %j inserts jobid
