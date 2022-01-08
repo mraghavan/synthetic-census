828d17482619b984cee03e2ffd094e56ba20a378
Manish Raghavan <mraghavan@holylogin01.rc.fas.harvard.edu>
2022-01-08 01:46:04 -0500

    out file changed

--- code/runscript.sh
+++ code/runscript.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH -c 4                # Number of cores (-c)
-#SBATCH -t 0-3:00          # Runtime in D-HH:MM, minimum of 10 minutes
+#SBATCH -t 0-2:00          # Runtime in D-HH:MM, minimum of 10 minutes
 #SBATCH -p serial_requeue   # Partition to submit to
 #SBATCH --mem=5000           # Memory pool for all cores (see also --mem-per-cpu)
-#SBATCH -o census.%A_%a.out  # File to which STDOUT will be written, %j inserts jobid
-#SBATCH -e census.%A_%a.err  # File to which STDERR will be written, %j inserts jobid
+#SBATCH -o out_files/census.%A_%a.out  # File to which STDOUT will be written, %j inserts jobid
+#SBATCH -e out_files/census.%A_%a.err  # File to which STDERR will be written, %j inserts jobid
 #SBATCH --array=1-5
 #SBATCH --mail-type=END
 # Make sure the array end is the same as the number passed to partition_blocks
