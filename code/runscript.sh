ad1512680d7a347077566928c66057d68befc351
Manish Raghavan <mraghavan@holylogin01.rc.fas.harvard.edu>
2022-01-08 02:36:01 -0500

    lower reqs

--- code/runscript.sh
+++ code/runscript.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH -c 4                # Number of cores (-c)
-#SBATCH -t 0-1:00          # Runtime in D-HH:MM, minimum of 10 minutes
+#SBATCH -t 0-0:30          # Runtime in D-HH:MM, minimum of 10 minutes
 #SBATCH -p serial_requeue   # Partition to submit to
-#SBATCH --mem=5000           # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH --mem=1000           # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH -o out_files/census.%A_%a.out  # File to which STDOUT will be written, %j inserts jobid
 #SBATCH -e out_files/census.%A_%a.err  # File to which STDERR will be written, %j inserts jobid
 #SBATCH --array=1-10
