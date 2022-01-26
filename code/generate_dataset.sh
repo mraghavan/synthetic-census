da0e6d1449b430a10a68d710f0ea8bc06895a44a
Manish Raghavan <manishraghavan@gmail.com>
2022-01-26 11:53:11 -0500

    readability

--- code/generate_dataset.sh
+++ code/generate_dataset.sh
@@ -1,2 +1,4 @@
 #/bin/bash
-JOB_ID=$(sbatch --parsable runscript.sh) && echo $JOB_ID && sbatch --dependency=afterok:$JOB_ID sample_script.sh $JOB_ID
+JOB_ID=$(sbatch --parsable runscript.sh) || exit 1
+echo $JOB_ID
+sbatch --dependency=afterok:$JOB_ID sample_script.sh $JOB_ID
