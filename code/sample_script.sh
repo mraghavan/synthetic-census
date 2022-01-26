eb56998f54b96e9f999bb105194607140fb4724b
Manish Raghavan <manishraghavan@gmail.com>
2022-01-26 14:11:25 -0500

    updated reqs

--- code/sample_script.sh
+++ code/sample_script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH -c 1                # Number of cores (-c)
-#SBATCH -t 0-1:00          # Runtime in D-HH:MM, minimum of 10 minutes
+#SBATCH -t 0-0:30          # Runtime in D-HH:MM, minimum of 10 minutes
 #SBATCH -p serial_requeue   # Partition to submit to
-#SBATCH --mem=50000           # Memory pool for all cores (see also --mem-per-cpu)
-#SBATCH -o out_files/samp.out  # File to which STDOUT will be written, %j inserts jobid
-#SBATCH -e out_files/samp.err  # File to which STDERR will be written, %j inserts jobid
+#SBATCH --mem=1000           # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH -o out_files/samp.%j.out  # File to which STDOUT will be written, %j inserts jobid
+#SBATCH -e out_files/samp.%j.err  # File to which STDERR will be written, %j inserts jobid
 #SBATCH --mail-type=END
 module load python/3.8.5-fasrc01
 python3 sample_from_dist.py $1
