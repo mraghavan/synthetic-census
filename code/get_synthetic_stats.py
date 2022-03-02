937abd0bc266adcad906951028209426ade9ed27
Manish Raghavan <manishraghavan@gmail.com>
2022-03-02 10:25:48 -0500

    fixed bug, added size adjusted tvd

--- code/get_synthetic_stats.py
+++ code/get_synthetic_stats.py
@@ -3,7 +3,6 @@ from knapsack_utils import *
 import pandas as pd
 from build_micro_dist import read_microdata
 from collections import Counter
-from guided_solver import to_sol_1
 import sys
 
 NON_BLOCK_COLS_L = [
@@ -45,10 +44,45 @@ def get_block_df(df):
     return block_df
 
 def process_dist(dist):
-    new_dist = {}
+    new_dist = Counter()
     for k, v in dist.items():
-        new_dist[k.race_counts + (k.eth_count,) + (k.n_over_18,)] = v
+        new_dist[k.race_counts + (k.eth_count,) + (k.n_over_18,)] += v
     return new_dist
+
+def hh_size(tup):
+    return sum(tup[:len(Race)])
+
+def get_size_dist(dist):
+    size_dist = Counter()
+    for hh, prob in dist.items():
+        size_dist[hh_size(hh)] += prob
+    return size_dist
+
+def size_filter(dist, size):
+    filtered = Counter()
+    for hh, prob in dist.items():
+        if hh_size(hh) == size:
+            filtered[hh] += prob
+    if len(filtered) == 0:
+        return {}
+    else:
+        return normalize(filtered)
+
+def size_adjusted_tvd(d1, d2):
+    # Get size distribution from d1
+    size_dist = get_size_dist(d1)
+    distances = {}
+    for s in size_dist:
+        d1_filtered = size_filter(d1, s)
+        d2_filtered = size_filter(d2, s)
+        if not d1_filtered or not d2_filtered:
+            distances[s] = 1
+        else:
+            distances[s] = tvd(d1_filtered, d2_filtered)
+    tot = 0
+    for s in size_dist:
+        tot += size_dist[s] * distances[s]
+    return tot
 
 def test_representativeness(df):
     fallback_dist = process_dist(read_microdata(get_micro_file()))
@@ -58,9 +92,11 @@ def test_representativeness(df):
     for ind, row in counts.iterrows():
         hh_counts = tuple(row[HH_COLS].tolist())
         hh_dist[hh_counts] += row[-1]
-    print(Counter(normalize(hh_dist)).most_common(10))
+    hh_dist = normalize(hh_dist)
+    print(Counter(hh_dist).most_common(10))
     print(Counter(fallback_dist).most_common(10))
-    return tvd(hh_dist, fallback_dist)
+    print(get_size_dist(fallback_dist))
+    return tvd(hh_dist, fallback_dist), size_adjusted_tvd(fallback_dist, hh_dist)
 
 if __name__ == '__main__':
     if len(sys.argv) >= 2:
@@ -77,5 +113,10 @@ if __name__ == '__main__':
     print('Blocks solved at accuracy = 1: %.2f%%' % (sum(block_df['ACCURACY'] == 1) / len(block_df) * 100))
     print('Blocks solved at accuracy = 2: %.2f%%' % (sum(block_df['ACCURACY'] == 2) / len(block_df) * 100))
     print('Blocks solved at accuracy = 3: %.2f%%' % (sum(block_df['ACCURACY'] == 3) / len(block_df) * 100))
-    tot_var_dist = test_representativeness(df)
+    tot_var_dist, size_adjusted = test_representativeness(df)
     print('TVD between PUMS and synthetic HH distributions:', tot_var_dist)
+    print('Size-adjusted TVD between PUMS and synthetic HH distributions:', size_adjusted)
+
+    tot_var_dist, size_adjusted = test_representativeness(df[df['ACCURACY'] == 1])
+    print('TVD between PUMS and synthetic HH distributions for ACCURACY == 1:', tot_var_dist)
+    print('Size-adjusted TVD between PUMS and synthetic HH distributions for ACCURACY == 1:', size_adjusted)
