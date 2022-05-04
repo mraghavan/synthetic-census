0f3bd402a3b579017474e58fac41e8c9ea3ff363
Manish Raghavan <manishraghavan@gmail.com>
2022-05-04 10:14:11 -0400

    added slurm swap script

---
+++ code/swap_script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -c 1                # Number of cores (-c)
+#SBATCH -t 0-4:00          # Runtime in D-HH:MM, minimum of 10 minutes
+#SBATCH -p serial_requeue   # Partition to submit to
+#SBATCH --mem=100000           # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH -o out_files/swap.%j.out  # File to which STDOUT will be written, %j inserts jobid
+#SBATCH -e out_files/swap.%j.err  # File to which STDERR will be written, %j inserts jobid
+#SBATCH --mail-type=END
+module load python/3.8.5-fasrc01
+python3 swapping.py
