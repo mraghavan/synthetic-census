6c3c4b2a1a10992a59411bf8dc0fa0eec9559755
Manish Raghavan <manishraghavan@gmail.com>
2022-06-15 08:23:38 -0400

    full dataset generation

--- code/generate_dataset.sh
+++ code/generate_dataset.sh
@@ -1,4 +1,8 @@
 #/bin/bash
-JOB_ID=$(sbatch --parsable runscript.sh) || exit 1
-echo $JOB_ID
-sbatch --dependency=afterok:$JOB_ID sample_script.sh $JOB_ID
+[ "$#" -eq 1 ] || { echo "No argument given" ; exit 1 }
+PART_ID=$(sbatch --parsable runscript.sh $1) || exit 2
+echo "Partition job: $PART_ID"
+SAMP_ID=$(sbatch --parsable --dependency=afterok:$PART_ID sample_script.sh $1) || exit 3
+echo "Sample job: $SAMP_ID"
+SWAP_ID=$(sbatch --parsable --dependency=afterok:$SAMP_ID swap_script.sh $1) || exit 4
+echo "Swap job: $SWAP_ID"
