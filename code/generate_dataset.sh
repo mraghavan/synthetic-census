397449d186d28ff7b74e9efdaae22873c794adb0
Manish Raghavan <manishraghavan@gmail.com>
2022-06-15 08:27:41 -0400

    typo

--- code/generate_dataset.sh
+++ code/generate_dataset.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-[ "$#" -eq 1 ] || { echo "No argument given" ; exit 1 }
+[ "$#" -eq 1 ] || { echo "No argument given" ; exit 1 ; }
 PART_ID=$(sbatch --parsable runscript.sh $1) || exit 2
 echo "Partition job: $PART_ID"
 SAMP_ID=$(sbatch --parsable --dependency=afterok:$PART_ID sample_script.sh $1) || exit 3
