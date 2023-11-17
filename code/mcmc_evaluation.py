381438986517f06974b5dff2222c9be49ed80076
Manish Raghavan <mragh@mit.edu>
2023-11-17 08:48:36 -0500

    try again

--- code/mcmc_evaluation.py
+++ code/mcmc_evaluation.py
@@ -1,19 +1,58 @@
 import pickle
 import matplotlib.pyplot as plt
+from config2 import ParserBuilder
+import re
+import os
+
+parser_builder = ParserBuilder(
+        {'state': True,
+         'micro_file': True,
+         'block_clean_file': True,
+         'synthetic_output_dir': False,
+         })
+
 
 if __name__ == '__main__':
+    parser_builder.parse_args()
+    print(parser_builder.args)
+    parser_builder.verify_required_args()
+    args = parser_builder.args
+
     results = None
-    with open('./mcmc_results.pkl', 'rb') as f:
-        results = pickle.load(f)
-    print(results.keys())
+    # open all the files
+    random_tvds = None
+    random_missing_masses = None
+    sample_size = None
+    with open(args.synthetic_output_dir + 'random_dists.pkl', 'rb') as f:
+        random_dists = pickle.load(f)
+        print(random_dists.keys())
+        random_tvds = random_dists['random_tvds']
+        random_missing_masses = random_dists['random_missing_masses']
+        sample_size = random_dists['sample_size']
+
+    all_tvds = {}
+    all_missing_masses = {}
+    for f in os.listdir(args.synthetic_output_dir):
+        # check if f matches mcmc_[0-9]+_[0-9]+.pkl
+        if re.match('mcmc_[0-9]+_[0-9]+.pkl', f):
+            with open(args.synthetic_output_dir + f, 'rb') as f:
+                results = pickle.load(f)
+                tvds = results['all_tvds']
+                missing_masses = results['all_missing_masses']
+                for k, v in tvds.items():
+                    all_tvds[k] = v
+                for k, v in missing_masses.items():
+                    all_missing_masses[k] = v
+    print(all_tvds)
+    print(all_missing_masses)
 
     # num_random_samples = results['num_random_samples']
     # print('Number of samples per test', num_random_samples)
     
-    random_tvds = results['random_tvds']
-    random_missing_masses = results['random_missing_masses']
-    all_tvds = results['all_tvds']
-    all_missing_masses = results['all_missing_masses']
+    # random_tvds = random_dists['random_tvds']
+    # random_missing_masses = results['random_missing_masses']
+    # all_tvds = results['all_tvds']
+    # all_missing_masses = results['all_missing_masses']
 
     # get 95% upper bound on random tvds
     random_tvds = sorted(random_tvds)
@@ -33,7 +72,7 @@ if __name__ == '__main__':
     all_ks = sorted(list(set([k for _, k in all_tvds])))
     all_num_iterations = sorted(list(set([num_iterations for num_iterations, _ in all_tvds])))
     for k in all_ks:
-        plt.errorbar([num_iterations for num_iterations, _ in all_tvds if k == _], [all_tvds[(num_iterations, k)] for num_iterations, _ in all_tvds if k == _], label='k={}'.format(k), yerr=tvd_interval)
+        plt.errorbar(all_num_iterations, [all_tvds[(num, k)] for num in all_num_iterations], label='k={}'.format(k), yerr=tvd_interval)
     plt.legend()
     plt.xlabel('Number of MCMC iterations')
     plt.ylabel('Total variation distance')
@@ -41,13 +80,13 @@ if __name__ == '__main__':
     plt.xscale('log')
     # make horizontal lines at tvd_interval
     plt.plot([min(all_num_iterations), max(all_num_iterations)], [tvd_upper, tvd_upper], color='black', linestyle='--')
-    plt.title('TVD, {} samples'.format(results['num_random_samples']))
+    plt.title('TVD, {} samples'.format(sample_size))
     plt.savefig('tvd_plot.png')
     plt.show()
 
     # make missing mass plot: one line for each k, x axis is num_iterations
     for k in all_ks:
-        plt.errorbar([num_iterations for num_iterations, _ in all_missing_masses if k == _], [all_missing_masses[(num_iterations, k)] for num_iterations, _ in all_missing_masses if k == _], label='k={}'.format(k), yerr=missing_mass_interval)
+        plt.errorbar(all_num_iterations, [all_missing_masses[(num, k)] for num in all_num_iterations], label='k={}'.format(k), yerr=missing_mass_interval)
     plt.legend()
     plt.xlabel('Number of MCMC iterations')
     plt.ylabel('Missing probability mass')
@@ -55,6 +94,6 @@ if __name__ == '__main__':
     plt.xscale('log')
     # make horizontal lines at missing_mass_interval
     plt.plot([min(all_num_iterations), max(all_num_iterations)], [missing_mass_upper, missing_mass_upper], color='black', linestyle='--')
-    plt.title('Missing probability mass, {} samples'.format(results['num_random_samples']))
+    plt.title('Missing probability mass, {} samples'.format(sample_size))
     plt.savefig('missing_mass_plot.png')
     plt.show()
--- code/mcmc_test.py
+++ code/mcmc_test.py
@@ -6,6 +6,7 @@ from build_micro_dist import read_microdata
 import multiprocessing as mp
 from mcmc_sampler import MCMCSampler
 import pickle
+from collections import OrderedDict
 
 parser_builder = ParserBuilder(
         {'state': True,
@@ -65,6 +66,33 @@ def get_missing_mass(d1: Counter, d2: Counter):
             missing_mass += d2[k]
     return missing_mass
 
+def get_random_dist_info(sol, sample_size_per_thread, pool, num_attempts_per_thread):
+    all_random_dists = get_dist_over_samples_pooled(sol, sample_size_per_thread, pool, num_attempts_per_thread)
+    counter_sol = Counter(sol)
+    counter_ds = [Counter(normalize(d)) for d in all_random_dists]
+    random_tvds = [get_tvd(d, counter_sol) for d in counter_ds]
+    random_missing_masses = [get_missing_mass(d, counter_sol) for d in counter_ds]
+    return {
+            'random_tvds': random_tvds,
+            'random_missing_masses': random_missing_masses,
+            'sample_size': sample_size_per_thread * pool._processes,
+            }
+
+def make_generate_samples(counts, dist, sample_size_per_thread, pool, counter_sol):
+    def generate_samples(num_iterations, k):
+        all_tvds = {}
+        all_missing_masses = {}
+        dists = pool.starmap(get_n_mcmc_samples, [(counts, dist, sample_size_per_thread, num_iterations, k)] * pool._processes) #type: ignore
+        mcmc_dist = sum(dists, Counter())
+        mcmc_dist = Counter(normalize(mcmc_dist))
+        all_tvds[(num_iterations, k)] = get_tvd(mcmc_dist, counter_sol)
+        all_missing_masses[(num_iterations, k)] = get_missing_mass(mcmc_dist, counter_sol)
+        return {
+                'all_tvds': all_tvds,
+                'all_missing_masses': all_missing_masses,
+                }
+    return generate_samples
+
 def main():
     parser_builder.parse_args()
     print(parser_builder.args)
@@ -75,46 +103,65 @@ def main():
     # non-empty rows
     df = df[df['H7X001'] > 0]
     print(df.head())
-    hh_dist = encode_hh_dist(read_microdata(args.micro_file))
+    dist = encode_hh_dist(read_microdata(args.micro_file))
     test_row = 3
-    pool = mp.Pool(mp.cpu_count())
-    sample_size_per_thread = 400
+    pool = mp.Pool(mp.cpu_count()//2)
+    sample_size_per_thread = 100
     print('num cpus', pool._processes) #type: ignore
     print('Sample size', sample_size_per_thread * pool._processes) #type: ignore
     row = df.iloc[test_row]
-    sol = solve(row, hh_dist)
+    sol = solve(row, dist)
+    print('cpu')
 
     ks = [2, 3, 4, 5]
     num_iterations = [1, 10, 100, 1000, 10000, 100000]
 
-    combos_to_test = [(i, k) for i in num_iterations for k in ks]
-    all_random_dists = get_dist_over_samples_pooled(sol, sample_size_per_thread, pool, num_attempts_per_thread=50)
-    print('Generated', len(all_random_dists), 'random samples of size', sum(all_random_dists[0].values()))
+    counts = encode_row(row)
     counter_sol = Counter(sol)
-    random_tvds = [get_tvd(Counter(normalize(d)), counter_sol) for d in all_random_dists]
-    random_missing_masses = [get_missing_mass(Counter(normalize(d)), counter_sol) for d in all_random_dists]
-    print('Got random TVDs', 'mean', np.mean(random_tvds), 'std', np.std(random_tvds))
-    all_tvds = {}
-    all_missing_masses = {}
-    for num_iterations, k in combos_to_test:
-        print('num_iterations', num_iterations, 'k', k)
-        dists = pool.starmap(get_n_mcmc_samples, [(encode_row(row), hh_dist, sample_size_per_thread, num_iterations, k)] * pool._processes) #type: ignore
-        mcmc_dist = sum(dists, Counter())
-        mcmc_dist = Counter(normalize(mcmc_dist))
-        all_tvds[(num_iterations, k)] = get_tvd(mcmc_dist, counter_sol)
-        all_missing_masses[(num_iterations, k)] = get_missing_mass(mcmc_dist, counter_sol)
-        print(all_tvds[(num_iterations, k)], all_missing_masses[(num_iterations, k)])
+    generate_samples = make_generate_samples(counts, dist, sample_size_per_thread, pool, counter_sol)
+    jobs = OrderedDict({
+            'random_dists.pkl': lambda: get_random_dist_info(sol, sample_size_per_thread, pool, num_attempts_per_thread=50),
+            })
+    for k in ks:
+        for ni in num_iterations:
+            jobs['mcmc_{}_{}.pkl'.format(ni, k)] = lambda ni=ni, k=k: generate_samples(ni, k) #type: ignore
+    jobs = {args.synthetic_output_dir + k: v for k, v in jobs.items()}
+    for job, func in jobs.items():
+        if os.path.exists(job):
+            print('Skipping', job)
+            continue
+        print('Running', job)
+        res = func()
+        with open(job, 'wb') as f:
+            print ('Writing', job)
+            pickle.dump(res, f)
+    # combos_to_test = [(i, k) for i in num_iterations for k in ks]
+    # all_random_dists = get_dist_over_samples_pooled(sol, sample_size_per_thread, pool, num_attempts_per_thread=50)
+    # print('Generated', len(all_random_dists), 'random samples of size', sum(all_random_dists[0].values()))
+    # random_tvds = [get_tvd(Counter(normalize(d)), counter_sol) for d in all_random_dists]
+    # random_missing_masses = [get_missing_mass(Counter(normalize(d)), counter_sol) for d in all_random_dists]
+    # print('Got random TVDs', 'mean', np.mean(random_tvds), 'std', np.std(random_tvds))
+    # all_tvds = {}
+    # all_missing_masses = {}
+    # for num_iterations, k in combos_to_test:
+        # print('num_iterations', num_iterations, 'k', k)
+        # dists = pool.starmap(get_n_mcmc_samples, [(encode_row(row), hh_dist, sample_size_per_thread, num_iterations, k)] * pool._processes) #type: ignore
+        # mcmc_dist = sum(dists, Counter())
+        # mcmc_dist = Counter(normalize(mcmc_dist))
+        # all_tvds[(num_iterations, k)] = get_tvd(mcmc_dist, counter_sol)
+        # all_missing_masses[(num_iterations, k)] = get_missing_mass(mcmc_dist, counter_sol)
+        # print(all_tvds[(num_iterations, k)], all_missing_masses[(num_iterations, k)])
 
-    with open (args.synthetic_output_dir + 'mcmc_results.pkl', 'wb') as f:
-        pickle.dump(
-                {
-                    'random_tvds': random_tvds,
-                    'random_missing_masses': random_missing_masses,
-                    'all_tvds': all_tvds,
-                    'all_missing_masses': all_missing_masses,
-                    'num_random_samples': sample_size_per_thread * pool._processes, #type: ignore
-                    },
-                f)
+    # with open (args.synthetic_output_dir + 'mcmc_results.pkl', 'wb') as f:
+        # pickle.dump(
+                # {
+                    # 'random_tvds': random_tvds,
+                    # 'random_missing_masses': random_missing_masses,
+                    # 'all_tvds': all_tvds,
+                    # 'all_missing_masses': all_missing_masses,
+                    # 'num_random_samples': sample_size_per_thread * pool._processes, #type: ignore
+                    # },
+                # f)
 
 if __name__ == '__main__':
     main()
