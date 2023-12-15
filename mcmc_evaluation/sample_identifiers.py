e0e14345dcfad951d64200856bd871ce3b95869c
Manish Raghavan <mragh@mit.edu>
2023-12-15 12:47:57 -0600

    sample

--- mcmc_evaluation/sample_identifiers.py
+++ mcmc_evaluation/sample_identifiers.py
@@ -3,6 +3,7 @@ import os
 import pickle
 import numpy as np
 import re
+from collections import OrderedDict
 sys.path.append('../')
 from syn_census.utils.config2 import ParserBuilder
 
@@ -23,7 +24,7 @@ def get_relevant_blocks(results_dir: str, task_name: str, max_num_sols: int):
     and their corresponding solution counts.
     """
     pattern = re.compile(rf'{task_name}_\d+_\d+.pkl')
-    sol_lens = {}
+    sol_lens = OrderedDict()
     for fname in os.listdir(results_dir):
         if pattern.match(fname):
             with open(results_dir + fname, 'rb') as f:
@@ -41,9 +42,12 @@ if __name__ == '__main__':
     max_hh = 20
     sample_size = args.num_samples
     sol_lens = get_relevant_blocks(args.synthetic_output_dir, args.task_name, args.num_sols)
+    np.random.seed(123)
+    filtered_sol_ids = sorted([k for k, v in sol_lens.items() if min_hh <= v <= max_hh])
     filtered_sol_lens = {k: v for k, v in sol_lens.items() if min_hh <= v <= max_hh}
     print('Number of blocks with between {} and {} hhs: {}'.format(min_hh, max_hh, len(filtered_sol_lens)))
-    random_sample_of_block_ids = set(np.random.choice(list(filtered_sol_lens.keys()), size=sample_size, replace=False))
+    random_permutation = np.random.permutation(filtered_sol_ids)
+    random_sample_of_block_ids = random_permutation[:sample_size]
     print(random_sample_of_block_ids)
     out_file = os.path.join(args.mcmc_output_dir, 'sampled_block_ids.txt')
     with open(out_file, 'w') as f:
