64a517629f794a66c8c1df0ded17de8c3dc4f840
Manish Raghavan <mragh@mit.edu>
2023-12-22 16:19:39 -0500

    param arg

--- shard_generation.sh
+++ shard_generation.sh
@@ -7,12 +7,13 @@
 #SBATCH -e out_files/census.%A_%a.err  # File to which STDERR will be written, %j inserts jobid
 #SBATCH --array=1-100
 #SBATCH --mail-type=END
-if [ "$#" -eq 1 ]; then
-    TASK_NAME=$1
+if [ "$#" -eq 2 ]; then
+    PARAM_FILE="$1"
+    TASK_NAME=$2
 else
-    echo "No task name given. Echo defaulting to job id"
-    TASK_NAME=$SLURM_ARRAY_JOB_ID
+    echo Missing arguments PARAM_FILE or TASK_NAME
+    exit 1
 fi
 module load sloan/python/modules/python-3.6/gurobipy/9.0.1
 python3 -m pip install gurobipy
-python3 generate_data_shard.py --from_params AL_params.json --task $SLURM_ARRAY_TASK_ID --num_tasks $SLURM_ARRAY_TASK_COUNT --task_name $TASK_NAME
+python3 generate_data_shard.py --from_params "$PARAM_FILE" --task $SLURM_ARRAY_TASK_ID --num_tasks $SLURM_ARRAY_TASK_COUNT --task_name $TASK_NAME
