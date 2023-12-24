811d9fc19c4bc21a1f00e5590c321883a48edf8b
Manish Raghavan <mragh@mit.edu>
2023-12-23 17:53:29 -0800

    take param argument

--- mcmc_evaluation/build_graphs.sh
+++ mcmc_evaluation/build_graphs.sh
@@ -7,9 +7,16 @@
 #SBATCH -e out_files/build.%A_%a.err  # File to which STDERR will be written, %j inserts jobid
 #SBATCH --array=1-50
 #SBATCH --mail-type=END
+#
+if [ "$#" -eq 1 ]; then
+    PARAM_FILE="$1"
+else
+    echo Missing argument PARAM_FILE
+    exit 1
+fi
 
 module load sloan/python/modules/python-3.6/gurobipy/9.0.1
 python3 -m pip install gurobipy
 python3 -m pip install --user networkx
-python3 build_graphs.py --from_params ../AL_params.json --task $SLURM_ARRAY_TASK_ID --num_tasks $SLURM_ARRAY_TASK_COUNT
-python3 analyze_graphs.py --from_params ../AL_params.json --task $SLURM_ARRAY_TASK_ID --num_tasks $SLURM_ARRAY_TASK_COUNT
+python3 build_graphs.py --from_params "$PARAM_FILE" --task $SLURM_ARRAY_TASK_ID --num_tasks $SLURM_ARRAY_TASK_COUNT
+python3 analyze_graphs.py --from_params "$PARAM_FILE" --task $SLURM_ARRAY_TASK_ID --num_tasks $SLURM_ARRAY_TASK_COUNT
