26fdc94133d2e7b5ba2a44e2de98a0677cb82b03
Manish Raghavan <mragh@mit.edu>
2024-04-21 12:42:02 -0400

    dist adjustments, cleanup

--- syn_census/synthetic_analysis/representativeness.py
+++ syn_census/synthetic_analysis/representativeness.py
@@ -102,10 +102,6 @@ def size_race_adjusted_tvd(d1, d2):
         else:
             distances[s] = tvd(d1_filtered, d2_filtered)
     tot = 0
-    # print('1 person', size_dist[1], distances[1], file=sys.stderr)
-    # ss = sorted(s for s in size_dist)
-    # plt.bar(ss, [size_dist[s] * distances[s] for s in ss])
-    # plt.show()
     for s in size_dist:
         tot += size_dist[s] * distances[s]
     return tot
@@ -123,7 +119,6 @@ def adjust_race(d1, d2):
 def adjust_ethnicity(d1, d2):
     h1_mass = get_h_mass(d1)
     h2_mass = get_h_mass(d2)
-    # print(h1_mass, h2_mass, file=sys.stderr)
     new_d2 = {}
     for hh, prob in d2.items():
         hh_eth = get_eth_size_one_hh(hh)
@@ -159,10 +154,6 @@ def test_representativeness(df, fallback_dist):
         hh_counts = tuple(row[HH_COLS].tolist())
         hh_dist[hh_counts] += row[-1]
     hh_dist = normalize(hh_dist)
-    # print(Counter(hh_dist).most_common(5), file=sys.stderr)
-    # print(Counter(fallback_dist).most_common(5), file=sys.stderr)
-    # print('hh_dist size', get_size_dist(hh_dist), file=sys.stderr)
-    # print('fallback_dist size', get_size_dist(fallback_dist), file=sys.stderr)
     test_mixed_race(hh_dist, fallback_dist)
     return tvd(hh_dist, fallback_dist), size_race_adjusted_tvd(hh_dist, fallback_dist)
 
@@ -171,9 +162,6 @@ def test_mixed_race(hh_dist, fallback_dist):
     fb_mr = dist_to_mr(fallback_dist)
     add_tex_var('SynMRPct', hh_mr[True] * 100)
     add_tex_var('PUMSMRPct', fb_mr[True] * 100)
-    # print('hh_mr', hh_mr, file=sys.stderr)
-    # print('fb_mr', fb_mr, file=sys.stderr)
-    # TODO
 
 def dist_to_mr(dist):
     mr_dist = Counter()
@@ -213,10 +201,7 @@ def print_results(
         tag: str = '',
         ):
     df = load_synthetic(synthetic_data)
-    # print(df.head())
-    # print('Number of households:', len(df), file=sys.stderr)
     add_tex_var('TotalHH', len(df))
-    # print('Total population:', sum(df['TOTAL']), file=sys.stderr)
     add_tex_var('TotalPop', sum(df['TOTAL']))
     block_df = get_block_df(df)
     add_tex_var('NumBlocks', len(block_df))
@@ -237,7 +222,7 @@ def print_results(
 
     print_all_tex_vars(state + tag)
 
-def get_adjustments(df, fallback_dist, smoothing=0.001):
+def get_adjustments(df, fallback_dist, smoothing=0.001, mr_mult=2.0):
     hh_dist = Counter()
     hh_df = df[HH_COLS]
     counts = hh_df.groupby(list(hh_df.columns)).size().reset_index(name='counts')
@@ -245,8 +230,6 @@ def get_adjustments(df, fallback_dist, smoothing=0.001):
         hh_counts = tuple(row[HH_COLS].tolist())
         hh_dist[hh_counts] += row[-1]
     hh_dist = normalize(hh_dist)
-    # return tvd(hh_dist, fallback_dist), size_race_adjusted_tvd(hh_dist, fallback_dist)
-# def tvd(d1, d2):
     all_keys = set(hh_dist.keys())
     all_keys = all_keys.union(set(fallback_dist.keys()))
     c1 = Counter(normalize(hh_dist))
@@ -255,7 +238,11 @@ def get_adjustments(df, fallback_dist, smoothing=0.001):
     for key in all_keys:
         c1[key] += smoothing
         c2[key] += smoothing
+        if c1[key] == 0:
+            continue
         ratios[key] = c2[key] / c1[key]
+        if is_mixed_race(key):
+            ratios[key] *= mr_mult
     return ratios
 
 def write_dist_adjustment(
